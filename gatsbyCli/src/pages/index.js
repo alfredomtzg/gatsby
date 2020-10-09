ecae4e356af4b6e94e7ca1770b99d3bcb72be3b8
Alfredo Martinez <psialfredomtz@gmail.com>
2020-10-09 15:58:26 -0500

    Change Text

--- gatsbyCli/src/pages/index.js
+++ gatsbyCli/src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi people I'm Alfredo</h1>
+    <p>Welcome to  new Gatsby site whit npx Garsby </p>
+    <p>Now go build something great or not, is your choice </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
